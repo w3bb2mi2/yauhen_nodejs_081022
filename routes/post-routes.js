b561a6da648c4d7753a86850dabdf11929933615
Eduard <w3bb2mi2@gmail.com>
2022-10-11 19:24:05 +0300

    control commit

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -1,17 +1,11 @@
 const express = require("express")
 const router = express.Router()
 
-const methodOverride = require("method-override");
-const { getSinglePost, getPosts, getAddPost, addPost, editPost, deletePost, getEditPost } = require("../controllers/post-controller");
+const { getSinglePost, getPosts, getAddPost, addPost, editPost, deletePost, getEditPost, homePage } = require("../controllers/post-controller");
 
-router.use(methodOverride("_method"))
 
-router.get("/", (req, res) => {
-    res.render(createPath("index"))
-});
-
+router.get("/", homePage);
 router.get("/posts", getPosts);
-
 router.get("/posts/:id", getSinglePost)
 router.get("/edit/:id", getEditPost)
 router.delete("/posts/:id", deletePost);
